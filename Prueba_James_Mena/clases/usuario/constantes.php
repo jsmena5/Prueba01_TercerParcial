5cde522a9011b90a46a627d5ee33cd26522a302b
Mena Perez, James Steve <jsmena5@espe.edu.ec>
2025-02-14 00:13:44 -0500

    Delete Prueba_James_Mena directory

--- Prueba_James_Mena/clases/usuario/constantes.php
+++
@@ -1,7 +0,0 @@
-<?php
-//siempre usar la variable de constantes para tener apicar en proximos casos 
-  define("SERVER","localhost");
-  define("USER","root");
-  define("PASS","Dm0986759875");
-  define("BD","usuariosbd");
- ?>
